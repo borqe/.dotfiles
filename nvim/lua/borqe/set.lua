9334d3c6cb1e5a7b63ce6b2b088d8498804b4952
Robertas Paulauskas <r.paulauskas@protonmail.com>
2023-10-10 10:42:03 +0300

    helper chars for nvim

--- nvim/lua/borqe/set.lua
+++ nvim/lua/borqe/set.lua
@@ -1,25 +1,25 @@
+-- Note for myself: set == vim.opt.
+--
 vim.opt.guicursor = ""
-
+-- Numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
+-- Tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
-
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
+-- :shrug:
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
+-- Helper characters 
+vim.opt.listchars = { space = '_', tab = '>~', eol = '↵' }
+vim.opt.list = true
 
