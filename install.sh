1bd97909e6ebe1add62baf84b1f1c31a65f3d81b
Robertas Paulauskas <r.paulauskas@protonmail.com>
2024-05-29 23:12:38 +0300

    some more battle testing

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-#
+
 # VARIABLES
+
 REPO_PATH="/mnt/c/users/robertas/repos"
 
 # Install applications
@@ -8,9 +9,12 @@ sudo apt install build-essential
 
 # install homebrew 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# for Windows:
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/user/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+# install necessities
 brew install zsh
 brew install neovim
 brew install ripgrep
@@ -24,7 +28,13 @@ ln -sf $REPO_PATH ~/repos
 
 # INSTALL ZSH
 zsh --version
-chsh -s /bin/zsh
+
+# for macOS/linux:
+# chsh -s /bin/zsh
+
+# for WSL:
+command -v zsh | sudo tee -a /etc/shells
+sudo chsh -s "$(command -v zsh)" "${USER}"
 
 # DOTFILES
 # TODO: get all files with a .dot extension and make symbolic links
@@ -35,4 +45,8 @@ ln -sf $REPO_PATH/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf $REPO_PATH/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf $REPO_PATH/.dotfiles/.zshrc ~/.zshrc
 
+# For Windows:
 find $REPO_PATH/.dotfiles -type f -exec dos2unix {} \;
+
+echo "Setting up aliases:"
+./aliases.sh
